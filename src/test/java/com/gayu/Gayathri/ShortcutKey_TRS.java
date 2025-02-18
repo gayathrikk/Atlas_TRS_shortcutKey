657081652f6b2a65a86d1a53c71242a01fa71ca7
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-02-18 15:38:19 +0530

    ShortcutKey_TRS.java

--- src/test/java/com/gayu/Gayathri/ShortcutKey_TRS.java
+++ src/test/java/com/gayu/Gayathri/ShortcutKey_TRS.java
@@ -33,10 +33,10 @@ public class ShortcutKey_TRS {
 			driver.manage().window().maximize();
 			System.out.println("--------------------------*****************-----------------------");
 			System.out.println("The server is Opened sucessfully");
-			WebDriverWait wait = new WebDriverWait(driver, 50);
-			WebElement viewerSectionLink = wait
-					.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/viewer/assets/images/colorsvg/gallery.svg']")));
-			viewerSectionLink.click();
+			// WebDriverWait wait = new WebDriverWait(driver, 50);
+			// WebElement viewerSectionLink = wait
+			// 		.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/viewer/assets/images/colorsvg/gallery.svg']")));
+			// viewerSectionLink.click();
 			System.out.println("--------------------------*****************-----------------------");
 			System.out.println("The Viewer Icon is clicked");
 			String parentWindow = driver.getWindowHandle();
